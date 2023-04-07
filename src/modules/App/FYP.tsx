bcc9e561b3e691884a183890014e898a5ff253af
Timothy Ganoza <jostimian@gmail.com>
2023-04-07 21:31:22 +0800

    I am so tired

--- src/modules/App/FYP.tsx
+++ src/modules/App/FYP.tsx
@@ -5,7 +5,7 @@ export const FYP = () => {
   return (
     <div className="w-full h-full flex items-center justify-center mt-10 flex-col gap-2">
       <ListBulletIcon className="h-28 w-28" />
-      <span className="font-bold text-xl">For You!</span>
+      <span className="font-bold text-xl">For You</span>
       <span>Active events you joined will appeare here!</span>
       <Button className="mt-5">Join Event</Button>
     </div>
